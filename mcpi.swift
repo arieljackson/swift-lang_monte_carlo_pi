93a3c6a6d2c1cba93b5a40df66f42b01d9dfa9c8
Ubuntu <ubuntu@ip-172-31-9-1.us-west-2.compute.internal>
2016-06-24 00:03:35 +0000

    incorporated visualization of results

--- mcpi.swift
+++ mcpi.swift
@@ -5,11 +5,13 @@ type file;
 
 //int throws   = 5000;
 int throws   = parseInt(arg("throws","5000")); 
-int nsims   = parseInt(arg("nsims", "10")); 
+int nsims   = parseInt(arg("nsims", "100")); 
  
 file out 			<"average.out">;
+file pic <"hello.png">;
 file mcpi_script	<"mcpi.sh">;
 file stats_script	<"stats.sh">;
+file viz_script <"viz.sh">;
 
 
 # ------- APP DEFINITIONS -------#
@@ -24,10 +26,16 @@ app (file o) analyze (file stats_script, file s[])
   bash filename(stats_script) filenames(s) stdout=filename(o);
 }
 
+app (file o) visualize (file viz_script, float av, file s[])
+{
+  bash filename(viz_script) av filenames(s);
+}
+
 # ----- WORKFLOW ELEMENTS ------#
 
 file sims[];
 float nums[];
+float average;
 
 foreach i in [0:nsims-1] { /* can change to nsims later */
   file simout <single_file_mapper; file=strcat("output/sim_",i,".out")>;
@@ -36,5 +44,11 @@ foreach i in [0:nsims-1] { /* can change to nsims later */
   nums[i] = read(sims[i]);
 }
 
+// iterate i {
+//     result = strcat(nums[i], " ");
+// } until (i == nsims);
+
+average = avg(nums);
 //out = analyze(stats_script,sims);
- out = write(avg(nums));
+ out = write(average);
+pic = visualize(viz_script, average, sims);
